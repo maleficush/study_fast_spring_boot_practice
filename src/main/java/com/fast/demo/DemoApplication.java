9dbb4fb1900fef7a72ec915f539076acd42add6f
maleficush <maleficush@gmail.com>
2022-01-17 14:06:24 +0900

    #ADD spring-cloud config server 테스트
    spring cloud config server 테스트

--- src/main/java/com/fast/demo/DemoApplication.java
+++ src/main/java/com/fast/demo/DemoApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
@@ -19,7 +20,7 @@ import org.springframework.core.env.Environment;
 import javax.annotation.PostConstruct;
 
 
-
+@EnableConfigServer
 @ConfigurationPropertiesScan("com.fast.demo.properties")
 @SpringBootApplication
 public class DemoApplication {
