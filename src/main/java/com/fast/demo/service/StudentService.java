107fc5d4335dc875ce0a4e36c307e420dd44c1af
maleficush <maleficush@gmail.com>
2022-01-17 13:16:55 +0900

    # ADD testcontainer 테스트
    redis docker container 테스트

--- src/main/java/com/fast/demo/service/StudentService.java
+++ src/main/java/com/fast/demo/service/StudentService.java
@@ -4,12 +4,14 @@ import com.fast.demo.domain.Student;
 import com.fast.demo.repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class StudentService {
     private final StudentRepository studentRepository;
 
@@ -21,6 +23,9 @@ public class StudentService {
     // 이 클래스의 빈 주입이 완료되었을 경우 호출
     @PostConstruct
     public void init(){
+        log.info("redis test data jack 생성");
         studentRepository.enroll("jack", 15, Student.Grade.B);
+        studentRepository.enroll("cassie", 18, Student.Grade.A);
+        studentRepository.enroll("fred", 14, Student.Grade.C);
     }
 }
